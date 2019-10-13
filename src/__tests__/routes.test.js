abecf58dbd002a08d00ae3c837df119965c5fcb3
JacobKnaack <jacobknck@gmail.com>
2019-10-12 22:35:45 -0700

    adds singin test

--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -11,10 +11,10 @@ afterAll(mockDB.stopDB);
 
 describe('Tests the router', () => {
   describe('User route', () => {
-    it('Creates returns a token on success', (done) => {
-      const testUser = {email: 'tester@test.com', password: 'test'};
-      request
-        .post('/api/signup')
+    const testUser = {email: 'tester@test.com', password: 'test'};
+
+    it('Creates a token on successful sign-up', (done) => {
+      request.post('/api/signup')
         .set('Content-Type', 'application/json')
         .send(testUser)
         .expect(200)
@@ -23,5 +23,15 @@ describe('Tests the router', () => {
           done();
         });
     });
+
+    it('Passes back a token on successful sign-in', (done) => {
+      request.post('/api/signin')
+        .auth(testUser.email, testUser.password)
+        .expect(200)
+        .then(response => {
+          expect(jwt.verify(response.text, process.env.API_SECRET)).toBeTruthy();
+          done();
+        })
+    })
   });
 });
